845dfb1bf3b3b47084cc98bf56d46620efaa28c0
QETHAN <qbylucky@gmail.com>
2013-12-06 20:52:16 +0800

    filters ng-dblclick

---
+++ angular/filters/demo.js
@@ -0,0 +1,6 @@
+/**
+ * Created by QETHAN on 13-12-6.
+ */
+var FilterCtrl = function($scope) {
+    $scope.num = 1234.5678;
+}
---
+++ angular/ngRepeat/demo.js
@@ -0,0 +1,12 @@
+/**
+ * Created by QETHAN on 13-12-6.
+ */
+var FriendsCtrl = function($scope) {
+
+    $scope.friends = [
+        { name: '男丁格爾', age: 18 },
+        { name: 'jelly', age: 16 },
+        { name: '梅干桑', age: 30 },
+        { name: '莫希爾', age: 31 }
+    ];
+}
