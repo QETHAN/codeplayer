de6b1b7c99c7eb4db9ffc92d819803c5d12f8f3a
QETHAN <qbylucky@gmail.com>
2013-12-08 12:05:35 +0800

    ngOptions

---
+++ angular/ngOptions/demo3.js
@@ -0,0 +1,19 @@
+/**
+ * Created by QETHAN on 13-12-6.
+ */
+var SelectCtrl = function($scope) {
+
+    $scope.onepiece = {
+        '船長': {
+            name: '蒙其·D·魯夫',
+            age: 19,
+            skills: ['橡膠槍', '橡膠火箭砲', '換檔', '霸氣']
+        }
+        // 略...
+        ,'音樂家': {
+            name: '布魯克',
+            age: 90,
+            skills: ['催眠曲·輪舞', '鼻唄三丁·矢筈斬', '冰凍劍', '哼歌·吹雪斬']
+        }
+    };
+};
