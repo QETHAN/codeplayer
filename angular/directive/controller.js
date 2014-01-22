ac3b7e48eb51875e52f6f090725085948f95ef7b
QETHAN <qbylucky@gmail.com>
2014-01-22 15:54:27 +0800

    jsonp

---
+++ angular/directive/controller.js
@@ -0,0 +1,23 @@
+/**
+ * Created by QETHAN on 14-1-22.
+ */
+
+//var appMoudule = angular.module('app',[]);
+//appMoudule.directive('ngbkFocus', function(){
+//	return {
+//		link: function(scope,element,attrs,controller){
+//			element[0].focus();
+//		}
+//	}
+//});
+function SomeController($scope) {
+	$scope.message = {text: 'nothing2 clicked yet'};
+
+	$scope.clickUnfocused = function() {
+		$scope.message = {text:'unfocused button clicked'};
+	}
+
+	$scope.clickFocused = function() {
+		$scope.message.text = 'focused button clicked';
+	}
+}
